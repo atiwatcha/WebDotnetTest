74c23a7af8719f552af6b83d2d7e5a5ac436a575
atiwat.chananet <atiwat.chananet@gmail.com>
2018-08-18 16:16:23 +0700

    Add project files.

---
+++ WebDotnetTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDotnetTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebDotnetTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebDotnetTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+           .UseUrls("http://*:5000") //Add this line of code.
+            .Build();
+
+            host.Run();
+        }
+    }
+}
